f602ec24ba4f92f0eb1f6b55cd304b2eb5fcc4b1
Ayane Nakamura <ayanakamu34@gmail.com>
2026-05-30 15:15:40 +0900

    5/30_lesson

---
+++ main.js
@@ -0,0 +1,24 @@
+
+
+const userName = document.querySelector( "#user-name" );
+const skillsList = document.querySelector( "#skills" );
+
+userName.textContent = "中村　綾音";
+
+const skills = [ "Java(基礎レベル)", "英語(海外で８年在住)" ];
+
+skills.forEach( skill => {
+    const item = document.createElement( 'div' );
+    item.className = 'skill-item';
+    TimeRanges.textContent = skill;
+    skillsList.appendChild( item );
+})
+
+const makeSkillsList = (skillsList, skills ) => {
+    skills.forEach( skill => {
+        const item = document.createElement( 'div' );
+        item.className = 'skill-item';
+        TimreRanges.textContent = skill;
+        skillsList.appendChild( item );
+    })
+}
